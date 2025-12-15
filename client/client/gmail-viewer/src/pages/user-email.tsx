1a85a8fd15c2941984746be123b6866c59f7d809
hashika2 <m.g.hashikamaduranga@gmail.com>
2025-12-15 13:21:15 +0530

    text change

--- client/client/gmail-viewer/src/pages/user-email.tsx
+++ client/client/gmail-viewer/src/pages/user-email.tsx
@@ -57,7 +57,7 @@ const UserEmail = () => {
                 <div className="relative">
                     <input
                     className="w-full bg-white placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md pl-3 pr-28 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-                    placeholder="UI Kits, Dashboards..." 
+                    placeholder="Search Emails..." 
                     />
                 </div>
             </div>
